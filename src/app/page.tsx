35aaa672a52a2274e2ea312dc358d2b6b32ea6dd
rodkev <rodbrin11@gmail.com>
2025-02-24 23:22:32 -0400

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+
+const words = `Bienvenidos a EGE`;
 
 export default function Home() {
   return (
     <div className="flex gap-2 justify-center">
+      <TextGenerateEffect words={words} />
       <Button asChild size="sm" variant={"outline"}>
         <Link href="/sign-in">Sign in</Link>
       </Button>
