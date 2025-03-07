414c27848efb08efc224c6da1750d86fd576452f
Alejandro Castellon <alejandrocastellonfer@gmail.com>
2025-03-07 17:18:48 -0400

    implement landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,303 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+
+{
+  /*pagina inicio */
+}
+const inicio =
+  "Gestiona tu gimnasio de manera inteligente desde cualquier lugar de forma fácil y segura con CM";
+
+const inicio1 =
+  "Accede a todas las herramientas que necesitas para administrar tu gimnasio, optimizar el seguimiento de tus clientes y hacer crecer tu negocio.  ";
+
+const frase1 = "Que es clubs manager?";
+const frase1p1 =
+  "Es una plataforma integral diseñada para la gestión eficiente de gimnasios y centros deportivos. Con Clubs Manager, puedes: ";
+const frase1p2 = "Administrar clientes y sus membresías.";
+const frase1p3 = "Controlar ingresos y métricas clave.";
+const frase1p4 = "Registrar check-ins diarios.";
+const frase1p5 = "Personalizar precios, horarios y servicios.";
+
+{
+  /*como funciona */
+}
+
+const pag3frase1 = "¿Como funciona Clubs manager?";
+const pag3frase1p1 =
+  "Regístrate y crea tu gimnasio, Configura el perfil de tu gimnasio con horarios, precios y servicios.";
+const pag3frase2p2 =
+  "Elige el plan ideal para ti, selecciona el plan que mejor se adapte a las necesidades de tu negocio";
+const pag3frase2p3 =
+  "Personaliza y gestiona, añade clientes, registra membresías, realiza check-ins diarios y lleva el control financiero.";
+const pag3frase2p4 =
+  "Analiza y crece, visualiza reportes detallados, sigue el progreso de tus ingresos y optimiza tus estrategias con datos en tiempo real.";
+
+{
+  /*pagina segunda */
+}
+
+const pag2frase1 = "¿Que recibes cuando eres parte de clubs manager?";
+
+const pag2frase2 = "Gestión administrativa";
+const pag2frase2p1 =
+  "Base de datos de clientes: registra y organiza información clave como nombres, correos, tipo de membresía y fechas de inicio y vencimiento.";
+const pag2frase2p2 =
+  "Control de membresías: seguimiento claro de membresías activas, próximas a vencer y expiradas.";
+const pag2frase2p3 =
+  "Historial de clientes: consulta el registro completo de cada cliente, desde pagos hasta check-ins.";
+
+const pag2frase3 = " Control de asistencias";
+const pag2frase3p1 =
+  "Registro de entradas: permite a los administradores llevar un control de las visitas diarias de cada cliente.";
+const pag2frase3p2 =
+  "Búsqueda rápida: localiza clientes por nombre o CI y marca su asistencia con un solo clic.";
+
+const pag2frase3p3 =
+  "Historial de accesos: visualiza quién ha ingresado y cuántas veces a lo largo del tiempo.";
+
+const pag2frase4 = "Gestión financiera";
+const pag2frase4p1 =
+  "Historial de pagos: lleva un registro claro de cada transacción, incluyendo fechas, montos y clientes asociados.";
+const pag2frase4p2 =
+  "Reportes financieros: accede a gráficos y métricas sobre ingresos mensuales y totales.";
+const pag2frase4p3 =
+  "Recordatorios de pago: identifica membresías próximas a vencer para comunicarte con los clientes a tiempo..";
+
+const pag2frase5 = "Analsis y reportes";
+const pag2frase5p1 =
+  "Panel de métricas clave: visualiza datos como el número total de clientes, membresías activas y próximos vencimientos.";
+const pag2frase5p2 =
+  " Gráficas interactivas: compara ingresos mensuales y el rendimiento financiero de tu gimnasio.";
+const pag2frase5p3 =
+  "Seguimiento de crecimiento: identifica patrones y toma decisiones informadas con base en datos en tiempo real.";
+
+{
+  /* servicios */
+}
+
+{
+  /*pagina cuarta */
+}
+
+const pag4frase1 = "Precios";
+
+const pag4frase2 = "Estandar";
+const pag4frase2p1 = "50 dls";
+const pag4frase2p2 = "70 miembros activos";
+const pag4frase2p3 = "Todas las características incluidas.";
+
+const pag4frase3 = "Plus";
+const pag4frase3p1 = "100 dls";
+const pag4frase3p2 = "150 miembros activos";
+const pag4frase3p3 = "Todas las características incluidas.";
+
+const pag4frase4 = "Profesional";
+const pag4frase4p1 = "145 dls";
+const pag4frase4p2 = "Miembros activos ilimitados";
+const pag4frase4p3 = "Todas las características incluidas.";
+
+const pag4frase5 = "Personalizado";
+const pag4frase5p1 = "Módulos a medida.";
+const pag4frase5p2 = "Todas las características incluidas.";
+const pag4frase5p3 = "Contáctanos.";
+
+{
+  /porque elegirnos/;
+}
+const frase2 = "¿Por que elegirnos?";
+const frase22 =
+  "En Clubs Manager, nacimos con una visión clara: simplificar la gestión de gimnasios y centros deportivos a través de la  tecnología. Sabemos lo desafiante que puede ser administrar clientes, membresías y finanzas, por eso creamos una plataforma intuitiva que combina eficiencia y simplicidad.";
+const frase2p1 = "Facil de usar";
+const frase2p11 =
+  "Una plataforma intuitiva, diseñada para que cualquier administrador pueda gestionar su gimnasio sin complicaciones.";
+const frase2p2 = "Adaptable";
+const frase2p22 =
+  "Perfecta para gimnasios de todos los tamaños, desde estudios pequeños hasta grandes centros deportivos.";
+const frase2p3 = "Datos seguridad";
+const frase2p33 =
+  "La información de tu negocio y de tus clientes está protegida con las mejores prácticas de seguridad digital.";
+
+const pag3frase3 = "!!Comienza ahora!!";
+const pag3frase3p1 =
+  "No esperes más para digitalizar y potenciar tu gimnasio. Elige el plan perfecto y lleva la gestión de tu negocio al siguiente nivel.";
+
+{
+  /*pagina quinta */
+}
+
+const pag5frase1 = "Contactanos para resolver tus dudas.";
+const pag5frase2 =
+  "Estamos aquí para responder tus preguntas, brindarte soporte y ofrecerte toda la información que necesitas sobre Clubs Manager.";
+const pag5frase3 =
+  "Si buscas optimizar la administración de tu gimnasio, controlar tus membresías y potenciar tus ingresos, no dudes en ponerte en contacto con nosotros.";
+const pag5frase4 = "whatsapp - 71442851";
+const pag5frase5 = "email - 8wYvD@example.com";
 
 export default function Home() {
   return (
-    <div className="flex gap-2 justify-center">
-      <Button asChild size="sm" variant={"outline"}>
-        <Link href="/sign-in">Sign in</Link>
-      </Button>
-      <Button asChild size="sm" variant={"default"}>
-        <Link href="/sign-up">Sign up</Link>
-      </Button>
+    <div className="bg-black text-white min-h-screen p-8">
+      <div className="fixed top-0 left-0 w-full bg-gray-500 h-20">
+        <div className="absolute top-4 right-4 flex gap-4">
+          <Button asChild size="lg" variant="outline">
+            <Link href="/sign-in">Sign in</Link>
+          </Button>
+          <Button asChild size="lg" variant="default">
+            <Link href="/sign-up">Sign up</Link>
+          </Button>
+        </div>
+      </div>
+      <section className="flex justify-between items-center gap-8 mt-24">
+        <h1 className="text-5xl font-bold max-w-lg leading-snug">{inicio}</h1>
+        <Image
+          src="/landing/dashboard.png"
+          alt="Clubs Manager"
+          width={600}
+          height={400}
+          className="rounded-lg"
+        />
+      </section>
+
+      <section className="flex justify-between items-center gap-8 mt-24">
+        <h1 className="text-xl text-gray-600 mt-4 max-w-lg">{inicio1}</h1>
+      </section>
+
+      <section className="mt-16">
+        <h2 className="text-4xl font-bold mb-4">{frase1}</h2>
+        <p className="text-xl max-w-2x1 leading-relaxed">{frase1p1}</p>
+        <p className="text-xl max-w-2x1 leading-relaxed">{frase1p2}</p>
+        <p className="text-xl max-w-2x1 leading-relaxed">{frase1p3}</p>
+        <p className="text-xl max-w-2x1 leading-relaxed">{frase1p4}</p>
+        <p className="text-xl max-w-2x1 leading-relaxed">{frase1p5}</p>
+      </section>
+
+      <section className="mt-16">
+        <h2 className="text-4xl font-bold mb-8">{pag3frase1}</h2>
+        <ul className="text-xl space-y-4">
+          <li>{pag3frase1p1}</li>
+          <li>{pag3frase2p2}</li>
+          <li>{pag3frase2p3}</li>
+          <li>{pag3frase2p4}</li>
+        </ul>
+      </section>
+
+      <section className="mt-16">
+        <h2 className="text-4xl font-bold mb-8">{pag2frase1}</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-2xl font-bold mb-4">{pag2frase2}</h3>
+            <ul className="text-lg">
+              <li>{pag2frase2p1}</li>
+              <li>{pag2frase2p2}</li>
+              <li>{pag2frase2p3}</li>
+            </ul>
+          </div>
+
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-2xl font-bold mb-4">{pag2frase3}</h3>
+            <ul className="text-lg">
+              <li>{pag2frase3p1}</li>
+              <li>{pag2frase3p2}</li>
+              <li>{pag2frase3p3}</li>
+            </ul>
+          </div>
+
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-2xl font-bold mb-4">{pag2frase4}</h3>
+            <ul className="text-lg">
+              <li>{pag2frase4p1}</li>
+              <li>{pag2frase4p2}</li>
+              <li>{pag2frase4p3}</li>
+            </ul>
+          </div>
+
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-2xl font-bold mb-4">{pag2frase5}</h3>
+            <ul className="text-lg">
+              <li>{pag2frase5p1}</li>
+              <li>{pag2frase5p2}</li>
+              <li>{pag2frase5p3}</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-16">
+        <h2 className="text-4xl font-bold mb-8">{pag4frase1}</h2>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+          <div className="bg-gray-800 p-6 rounded-lg text-center">
+            <h3 className="text-2xl font-bold mb-4">{pag4frase2}</h3>
+            <ul className="text-lg">
+              <li>{pag4frase2p1}</li>
+              <li>{pag4frase2p2}</li>
+              <li>{pag4frase2p3}</li>
+            </ul>
+          </div>
+
+          <div className="bg-gray-800 p-6 rounded-lg text-center">
+            <h3 className="text-2xl font-bold mb-4">{pag4frase3}</h3>
+            <ul className="text-lg">
+              <li>{pag4frase3p1}</li>
+              <li>{pag4frase3p2}</li>
+              <li>{pag4frase3p3}</li>
+            </ul>
+          </div>
+
+          <div className="bg-gray-800 p-6 rounded-lg text-center">
+            <h3 className="text-2xl font-bold mb-4">{pag4frase4}</h3>
+            <ul className="text-lg">
+              <li>{pag4frase4p1}</li>
+              <li>{pag4frase4p2}</li>
+              <li>{pag4frase4p3}</li>
+            </ul>
+          </div>
+
+          <div className="bg-gray-800 p-6 rounded-lg text-center">
+            <h3 className="text-2xl font-bold mb-4">{pag4frase5}</h3>
+            <ul className="text-lg">
+              <li>{pag4frase5p1}</li>
+              <li>{pag4frase5p2}</li>
+              <li>{pag4frase5p3}</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-16">
+        <h2 className="text-4xl font-bold mb-8">{frase2}</h2>
+        <p className="text-lg mt-2">{frase22}</p>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+          <div className="bg-gray-800 p-6 rounded-lg text-center">
+            <h3 className="text-2xl font-bold mb-2">{frase2p1}</h3>
+            <p className="text-lg">{frase2p11}</p>
+          </div>
+          <div className="bg-gray-800 p-6 rounded-lg text-center">
+            <h3 className="text-2xl font-bold mb-2">{frase2p2}</h3>
+            <p className="text-lg">{frase2p22}</p>
+          </div>
+          <div className="bg-gray-800 p-6 rounded-lg text-center">
+            <h3 className="text-2xl font-bold mb-2">{frase2p3}</h3>
+            <p className="text-lg">{frase2p33}</p>
+          </div>
+        </div>
+
+        <h3 className="text-3xl font-bold mt-8">{pag3frase3}</h3>
+        <p className="text-lg mt-2">{pag3frase3p1}</p>
+      </section>
+
+      <section className="mt-16">
+        <h2 className="text-4xl font-bold mb-8">{pag5frase1}</h2>
+        <p className="text-xl mb-4">{pag5frase2}</p>
+        <p className="text-lg mb-8">{pag5frase3}</p>
+
+        <div className="flex flex-col space-y-4">
+          <p>{pag5frase4}</p>
+          <p>{pag5frase5}</p>
+        </div>
+      </section>
+
+      {/* footer*/}
     </div>
   );
 }
